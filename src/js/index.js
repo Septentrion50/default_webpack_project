49d45b814021c9311dea6fd51d3344bbcfcc9cd4
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-30 17:06:57 +0200

    Removed test function

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,1 @@
 import '../sass/style.scss';
-
-const getPosts = async() => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    return data;
-}
-
-getPosts().then(posts => console.log(posts));
